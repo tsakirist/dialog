8bde99e57180dc5e4e0e611cb04fe791d32f629a
tsakirist <tsakiris@uth.gr>
2016-08-23 17:51:23 +0300

    Keep goin

--- client/components/dialog/dialog-controller.js
+++ client/components/dialog/dialog-controller.js
@@ -1,16 +1,17 @@
 class DialogController {
 
-    // In case we want to access some property of $scope we just assign it to this.$scope and we use it from html like {{prop}}
-    constructor($scope, $mdDialog, $http) {
+    constructor($scope, $mdDialog) {
         this.$scope = $scope;
         this.$mdDialog = $mdDialog;
         this.exportTypes = ['CSV', 'TypeArray'];
         this.types = ['1', '2', '3', '4', '5'];
-        this.sensors = ['0', '1', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144', '233', '377'];
+        this.sensors = ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144'];
+        this.selectedSensors = [];
+        this.selectedTypes = [];
     }
 
-    getSelectedExportTypes() {
-        return this.selectedExportTypes;
+    getSelectedExportType() {
+        return this.selectedExportType;
     }
 
     getSelectedTypes() {
@@ -21,14 +22,42 @@ class DialogController {
         return this.radio;
     }
 
+    getSelectedSensors() {
+        return this.selectedSensors;
+    }
+
+    addSensor(sensor) {
+        this.selectedSensors.push(sensor);
+    }
+
+    clearTypes() {
+        this.selectedTypes = undefined;
+    }
+
+    exportJson() {
+        const json = {
+            export: this.getSelectedExportType(),
+            types : this.getSelectedTypes(),
+            radio : this.getRadio()
+        };
+        if(this.radio === 'All') {
+            json.sensors = this.sensors;
+        } else {
+            json.sensors = this.getSelectedSensors();
+        }
+        this.dialogCancel();
+        //alert(JSON.stringify(json));
+        console.log(JSON.stringify(json));
+    }
+
     hide() {
         this.$mdDialog.hide();
     }
 
-    cancel() {
+    dialogCancel() {
         this.$mdDialog.cancel();
     }
 
 }
 
-DialogController.$inject = ['$scope', '$mdDialog', '$http'];
+DialogController.$inject = ['$scope', '$mdDialog'];
