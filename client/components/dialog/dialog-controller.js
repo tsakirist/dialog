45b07e513093b44651c2e13fd9165ac24c58d3d7
tsakirist <tsakiris@uth.gr>
2016-08-24 16:40:08 +0300

    Injected obj from mainCtrl, fixes to dialog.pug

--- client/components/dialog/dialog-controller.js
+++ client/components/dialog/dialog-controller.js
@@ -1,13 +1,27 @@
 class DialogController {
 
-    constructor($scope, $mdDialog) {
+    // Object includes the available options injected from the mainController
+    constructor($scope, $mdDialog, object) {
         this.$scope = $scope;
         this.$mdDialog = $mdDialog;
-        this.exportTypes = ['CSV', 'TypeArray'];
-        this.types = ['1', '2', '3', '4', '5'];
-        this.sensors = ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144'];
-        this.selectedSensors = [];
-        this.selectedTypes = [];
+        this.object = object;
+        console.log(this.object);
+        // this.exportTypes = ['CSV', 'TypeArray'];
+        // this.types = ['1', '2', '3', '4', '5'];
+        // this.sensors = ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144'];
+        // this.selectedSensors = [];
+        // this.selectedTypes = [];
+        // const currDate = new Date();
+        // this.minDate = new Date(
+        //     currDate.getFullYear(),
+        //     currDate.getMonth()-2,
+        //     currDate.getDate()
+        // );
+        // this.maxDate = new Date(
+        //     currDate.getFullYear(),
+        //     currDate.getMonth()+1,
+        //     currDate.getDate()
+        // );
     }
 
     getSelectedExportType() {
@@ -24,6 +38,14 @@ class DialogController {
 
     getSelectedSensors() {
         return this.selectedSensors;
+    }
+
+    getStartDate() {
+        return this.startDate;
+    }
+
+    getEndDate() {
+        return this.endDate;
     }
 
     addSensor(sensor) {
@@ -45,8 +67,9 @@ class DialogController {
         } else {
             json.sensors = this.getSelectedSensors();
         }
+        json.startDate = this.getStartDate();
+        json.endDate = this.getEndDate();
         this.dialogCancel();
-        //alert(JSON.stringify(json));
         console.log(JSON.stringify(json));
     }
 
@@ -60,4 +83,4 @@ class DialogController {
 
 }
 
-DialogController.$inject = ['$scope', '$mdDialog'];
+DialogController.$inject = ['$scope', '$mdDialog', 'object'];
--- client/components/main/main-controller.js
+++ client/components/main/main-controller.js
@@ -3,16 +3,43 @@ class MainController {
     constructor($scope, $mdDialog) {
         this.$scope = $scope;
         this.$mdDialog = $mdDialog;
+        const currDate = new Date();
+        const minDate = new Date(
+            currDate.getFullYear(),
+            currDate.getMonth()-2,
+            currDate.getDate()
+        );
+        const maxDate = new Date(
+            currDate.getFullYear(),
+            currDate.getMonth()+1,
+            currDate.getDate()
+        );
+        this.testObj = {
+            exportTypes : ['CSV', 'TypeArray'],
+            types : ['1', '2', '3', '4', '5'],
+            sensors : ['0', '1', '2', '3', '5', '8', '13', '21', '34', '55', '89', '144'],
+            date: {
+                minDate: minDate,
+                maxDate: maxDate
+            }
+        };
+        //Auto open dialog
+        //const options = this.createDialog(event);
+        //this.$mdDialog.show(options);
     }
 
     createDialog(event) {
         return ({
-            controller: DialogController,
+            parent: angular.element(document.body),
             templateUrl: 'templates/dialog',
             targetEvent: event,
-            parent: angular.element(document.body),
             clickOutsideToClose: true,
-            controllerAs: 'vm'
+            escapeToClose: true,
+            controller: DialogController,
+            controllerAs: 'vm',
+            locals: {
+                object: this.testObj
+            }
         });
     }
 
